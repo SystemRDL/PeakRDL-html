d533d49802ab892a0725f7fbf654ee59e9e87f57
Alex Mykyta <amykyta3@users.noreply.github.com>
2020-02-16 11:28:01 -0800

    Update BigInteger libary. Add 3rd party license attribution

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3 :: Only",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
