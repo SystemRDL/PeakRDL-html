d30b9cbf7f8d79d7e5d1473b686dbb169e5713d9
Alex Mykyta <amykyta3@users.noreply.github.com>
2022-05-07 22:20:28 -0700

    Add build timestamp and version to URL requests to force cache invalidation when re-generated. Prevents browser from mis-rendering content due to version tearing.

--- docs/js/nav.js
+++ docs/js/nav.js
@@ -31,7 +31,7 @@ function load_page(id, done_callback) {
     };
 
     try {
-        xhttp.open("GET", "content/" + get_node_uid(id) + ".html", true);
+        xhttp.open("GET", "content/" + get_node_uid(id) + ".html?ts=" + BUILD_TS, true);
         xhttp.send();
     } catch(error) {
         if(window.location.protocol == "file:"){
